555063b7ce27cb75aad878a0d73c954bb07f7940
Mihir Sood <43700647+mihirsood@users.noreply.github.com>
2020-06-23 15:35:49 +0530

    Create CountCompleteTreeNodes.java

---
+++ June Challenge/Week 4/Java/Day_23/CountCompleteTreeNodes.java
@@ -0,0 +1,23 @@
+// Problem : https://leetcode.com/problems/count-complete-tree-nodes/
+// Solution:
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root==null) return 0;
+        return (countNodes(root.left) + countNodes(root.right) + 1);
+    }
+}
