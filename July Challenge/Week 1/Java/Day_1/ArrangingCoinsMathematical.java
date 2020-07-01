510f77858f8b8b4c748ee1f2c0fa06d5966a82a9
Mihir Sood <43700647+mihirsood@users.noreply.github.com>
2020-07-01 15:33:13 +0530

    Create ArrangingCoinsMathematical.java

---
+++ July Challenge/Week 1/Java/Day_1/ArrangingCoinsMathematical.java
@@ -0,0 +1,7 @@
+//Problem:https://leetcode.com/problems/arranging-coins/
+//Solution:
+class Solution {
+    public int arrangeCoins(int n) {
+        return (int)(Math.sqrt(2*(long)n + 0.25)-0.5);
+    }
+}
