6960068e1bc84b1de0b877824ea78a6c69c20eaf
Mihir Sood <43700647+mihirsood@users.noreply.github.com>
2020-07-26 14:40:29 +0530

    Create AddDigitsLoop.java

---
+++ July Challenge/Week 4/Java/Day_26/AddDigitsLoop.java
@@ -0,0 +1,17 @@
+// Problem : https://leetcode.com/problems/add-digits/
+// Solution:
+class Solution {
+    public int addDigits(int num) {
+        int res = 0;
+        while(num>0){
+            res += num%10;
+            num/=10;
+            
+            if(res>=10 && num==0){
+                num=res;
+                res=0;
+            }
+        }
+        return res;
+    }
+}
