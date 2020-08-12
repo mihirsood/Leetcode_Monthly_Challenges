33a45bed302fa0221ee521a6842173bd5d83934f
Mihir Sood <43700647+mihirsood@users.noreply.github.com>
2020-08-13 04:05:15 +0530

    Create H-Index.java

---
+++ August Challenge/Week 2/Java/Day_11/H-Index.java
@@ -0,0 +1,22 @@
+// Problem : https://leetcode.com/problems/h-index/
+// Solution:
+class Solution {
+    public int hIndex(int[] citations) {
+        if(citations.length==0)
+            return 0;
+        Arrays.sort(citations);
+        int l = 0;
+        int r = citations.length-1;
+        int n = r + 1 ;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(citations[mid] >= n-mid ){
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return n - l;
+    }
+}
