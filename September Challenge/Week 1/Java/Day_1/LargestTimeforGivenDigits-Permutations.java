cb837a544716ccccc33d04e169d52b81279ee247
Mihir Sood <43700647+mihirsood@users.noreply.github.com>
2020-09-04 02:30:30 +0530

    Create LargestTimeforGivenDigits-Permutations.java

---
+++ September Challenge/Week 1/Java/Day_1/LargestTimeforGivenDigits-Permutations.java
@@ -0,0 +1,41 @@
+// Problem : https://leetcode.com/problems/largest-time-for-given-digits/
+// Solution: 
+
+class Solution {
+    private int max_time = -1;
+    
+    public String largestTimeFromDigits(int[] A) {
+        this.max_time = -1;
+        permutate(A, 0);
+        if (this.max_time == -1)
+            return "";
+        else return String.format("%02d:%02d", max_time / 60, max_time % 60);
+    }
+    
+    private void permutate(int[] array, int start) {
+        if (start == array.length) {
+            this.build_time(array);
+            return;
+        }
+        for (int i = start; i < array.length; ++i) {
+            this.swap(array, i, start);
+            this.permutate(array, start + 1);
+            this.swap(array, i, start);
+        }
+    }
+    
+    private void build_time(int[] perm) {
+        int hour = perm[0] * 10 + perm[1];
+        int minute = perm[2] * 10 + perm[3];
+        if (hour < 24 && minute < 60)
+            this.max_time = Math.max(this.max_time, hour * 60 + minute);
+    }
+
+    private void swap(int[] array, int i, int j) {
+        if (i != j) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
