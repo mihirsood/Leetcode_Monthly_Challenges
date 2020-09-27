9c6d6cb459c1149a5352de93edb619198982c325
Mihir Sood <43700647+mihirsood@users.noreply.github.com>
2020-09-28 01:35:49 +0530

    Create CarPooling-3.java

---
+++ September Challenge/Week 3/Java/Day_21/CarPooling-3.java
@@ -0,0 +1,20 @@
+// Problem : https://leetcode.com/problems/car-pooling/
+ // Solution:
+
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] timestamp = new int[1001];
+        for (int[] trip : trips) {
+            timestamp[trip[1]] += trip[0];
+            timestamp[trip[2]] -= trip[0];
+        }
+        int ued_capacity = 0;
+        for (int number : timestamp) {
+            ued_capacity += number;
+            if (ued_capacity > capacity) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
