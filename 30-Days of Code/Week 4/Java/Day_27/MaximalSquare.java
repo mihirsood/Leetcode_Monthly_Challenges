aa7234e9b185a91816c3ee99757ccafa58084410
Mihir Sood <43700647+mihirsood@users.noreply.github.com>
2020-10-30 03:13:58 +0530

    Create MaximalSquare.java

---
+++ 30-Days of Code/Week 4/Java/Day_27/MaximalSquare.java
@@ -0,0 +1,21 @@
+// Problem : https://leetcode.com/problems/maximal-square/
+// Solution:
+
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if(matrix==null || matrix.length==0 || matrix[0].length==0) return 0;
+        int max = 0;
+        int dp[][] = new int[matrix.length+1][matrix[0].length+1];
+        
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                if(matrix[i-1][j-1]=='1'){
+                    dp[i][j]=Math.min(dp[i-1][j-1], Math.min(dp[i-1][j],dp[i][j-1]))+1;
+                    max=Math.max(max,dp[i][j]);
+                }
+            }
+        }
+        
+        return max*max;
+    }
+}
